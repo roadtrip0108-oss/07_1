ec1b790b2588abfd59bc7225f1f0d1d7c3528708
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-10-19 20:39:24 +0900

    07_2

--- 07_1.c
+++ 07_1.c
@@ -3,15 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
-  
-  for(i=0; i<5; i++)
-  {
-           static int temp=1;
-           printf("temp=%d\n", temp);
-           temp++;
-  }
-  
-  system("PAUSE");	
-  return 0;
+    int i=10;
+    
+    printf("함수 호출 전i=%d\n", i);
+    i=inc(i);
+    printf("함수 호출 후i=%d\n", i); 
+    
+    system("PAUSE"); 
+    return 0;
 }
+    int inc(int counter)
+    {
+        counter++;
+        return counter; 
+    }
