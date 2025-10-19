2a0fe8db5752c8e5fa9d966f65cee7e51ac1fc52
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-10-19 20:47:18 +0900

    07_4

--- 07_1.c
+++ 07_1.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    int i=10;
-    
-    printf("함수 호출 전i=%d\n", i);
-    i=inc(i);
-    printf("함수 호출 후i=%d\n", i); 
-    
-    system("PAUSE"); 
-    return 0;
-}
-    int inc(int counter)
+int counter;
+
+    void set_counter()
     {
-        counter++;
-        return counter; 
+         counter = 20;
     }
+    
+    int main(int argc, char *argv[])
+    {
+        printf("counter=%d\n", counter);
+        set_counter();
+        printf("counter=%d\n", counter);
+        
+        system("PAUSE");
+        return 0;    
+    }
+
