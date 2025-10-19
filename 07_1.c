8866d4ba30cf58e37fa3fad3a99100505b8f552c
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-10-19 20:23:54 +0900

    07_1

---
+++ 07_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int flag=1;
+  int y;
+  
+  while (flag !=0)
+  {
+        int y;
+        y=3;
+        flag=0;
+  }
+  
+  y=4;
+  
+  system("PAUSE");	
+  return 0;
+}
