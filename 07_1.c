41082755312ac788fd5c547781a7e9f915e19500
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-10-19 20:32:09 +0900

    07_2

--- 07_1.c
+++ 07_1.c
@@ -3,17 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-  int flag=1;
-  int y;
+  int i;
   
-  while (flag !=0)
+  for(i=0; i<5; i++)
   {
-        int y;
-        y=3;
-        flag=0;
+           static int temp=1;
+           printf("temp=%d\n", temp);
+           temp++;
   }
-  
-  y=4;
   
   system("PAUSE");	
   return 0;
